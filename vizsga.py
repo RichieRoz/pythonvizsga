5db457d2e24ffe3e765a0e995ab50b06243a9b84
RichRoz <l13s80neptun@gde.hu>
2023-11-30 17:05:47 +0100

    1-2 agyas szoba

--- vizsga.py
+++ vizsga.py
@@ -17,3 +17,37 @@ class StandardSzoba(Szoba):
 # Példányosítás és használat
 standard_szoba = StandardSzoba(20000, 101)
 standard_szoba.info()
+
+from abc import ABC, abstractmethod
+
+class Szoba(ABC):
+    def __init__(self, ar, szobaszam):
+        self.ar = ar
+        self.szobaszam = szobaszam
+
+    @abstractmethod
+    def info(self):
+        pass
+
+class EgyagyasSzoba(Szoba):
+    def __init__(self, szobaszam):
+        super().__init__(15000, szobaszam)
+        self.egyagyas_agy = True
+
+    def info(self):
+        print(f"Egyágyas szoba #{self.szobaszam}, ár: {self.ar} Ft, egyágyas ágy: {self.egyagyas_agy}")
+
+class KetagyasSzoba(Szoba):
+    def __init__(self, szobaszam):
+        super().__init__(25000, szobaszam)
+        self.ketagyas_agy = True
+
+    def info(self):
+        print(f"Kétágyas szoba #{self.szobaszam}, ár: {self.ar} Ft, kétágyas ágy: {self.ketagyas_agy}")
+
+# Példányosítás és használat
+egyagyas_szoba = EgyagyasSzoba(201)
+ketagyas_szoba = KetagyasSzoba(302)
+
+egyagyas_szoba.info()
+ketagyas_szoba.info()
